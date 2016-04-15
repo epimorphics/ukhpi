2cc4909d580e6d75b01d358cd7d2843f714c3087
Ian Dickinson <i.j.dickinson@gmail.com>
2016-04-15 14:44:34 +0100

    Comment out test stubs

--- test/controllers/exploration_controller_test.rb
+++ test/controllers/exploration_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ExplorationControllerTest < ActionController::TestCase
-  def test_sanity
-    flunk "Need real tests"
-  end
+  # def test_sanity
+  #   flunk "Need real tests"
+  # end
 end
--- test/helpers/exploration_helper_test.rb
+++ test/helpers/exploration_helper_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ExplorationHelperTest < ActionView::TestCase
-  def test_sanity
-    flunk "Need real tests"
-  end
+  # def test_sanity
+  #   flunk "Need real tests"
+  # end
 end
