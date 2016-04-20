6389f4fe0bf2d5f9d6505ca8a2304c0f445fcebb
Ian Dickinson <i.j.dickinson@gmail.com>
2016-04-20 09:37:18 +0100

    Updated to latest version of DSD

--- test/models/data_model_test.rb
+++ test/models/data_model_test.rb
@@ -22,7 +22,7 @@ class SearchCommandTest < ActiveSupport::TestCase
   it "should retrieve the comments for each component" do
     dsd = DataModel.new
     comments = dsd.measures.map &:comment
-    comments.include?( "The percentage change in the average house price compared to the previous month." ).must_equal true
+    comments.include?( "The percentage change in the average house price compared to the same period twelve months earlier." ).must_equal true
   end
 
   it "should separate out the dimensions" do
