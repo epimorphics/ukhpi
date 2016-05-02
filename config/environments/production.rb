3ea151ddf679d9cc7d227c364612bf0cbe97763b
Ian Dickinson <i.j.dickinson@gmail.com>
2016-05-02 23:12:36 +0100

    Set relative root in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.relative_url_root = "/app/ukhpi"
+
 end
