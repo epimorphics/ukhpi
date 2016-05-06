dbeabe15ae0d7b73c45f8dbef0934997839d6d9d
Ian Dickinson <i.j.dickinson@gmail.com>
2016-05-06 16:14:53 +0100

    Extend QueryResults to return the date period covered by the results

--- app/assets/javascripts/query-results.js
+++ app/assets/javascripts/query-results.js
@@ -19,9 +19,23 @@ modulejs.define( "query-results", [
     },
 
     results: function() {
-      return _.map( this._data.results, function( result ) {
-        return new QueryResult( result );
-      } );
+      if (!this._results) {
+        this._results = _.map( this._data.results, function( result ) {
+          return new QueryResult( result );
+        } );
+      }
+
+      return this._results;
+    },
+
+    dateRange: function() {
+      var min = _.minBy( this.results(), function( r) { return r.periodDate().toDate(); } );
+      var max = _.maxBy( this.results(), function( r) { return r.periodDate().toDate(); } );
+
+      return [min.periodDate(),
+              max.periodDate()
+                 .add( 1, "month")
+                 .subtract( 1, "second" )];
     }
   } );
 
