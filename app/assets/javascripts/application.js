4a115674b694025d9249aa997e3d4e6d0c0f222f
Ian Dickinson <ian@epimorphics.com>
2016-05-12 14:09:56 +0100

    Fix for issue #2: stop IE11 breaking in JQuery.datatables due to extraneous string prototype functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,16 @@ function(
 ) {
   "use strict";
 
+  /** Issue https://github.com/epimorphics/ukhpi/issues/2
+   * Something is adding functions to String.prototype, but I'm
+   * not sure what. In any case, this causes a for..in loop in
+   * jQuery dataTables to explode with a 'doesn't respond to charAt
+   * message. The only workaround I have at the moment, unsatisfactorily,
+   * is to remove the additional functions from the String prototype
+   */
+  delete String.prototype.startsWith;
+  delete String.prototype.repeat;
+
   var controller = new Controller();
 
   return {
