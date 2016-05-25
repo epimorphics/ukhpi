2bc66190a944b3c70d7b0ca983dfcbd470ac0c11
Ian Dickinson <i.j.dickinson@gmail.com>
2016-05-25 15:50:27 +0100

    Initial version of switching to wide table view

--- app/assets/javascripts/data-table-view.js
+++ app/assets/javascripts/data-table-view.js
@@ -3,35 +3,47 @@
 modulejs.define( "data-table-view", [
   "lib/lodash",
   "lib/jquery",
+  "constants",
   "preferences",
   "aspects"
 ], function(
   _,
   $,
+  Constants,
   Preferences,
   Aspects
 ) {
   "use strict";
 
   var DataTableView = function() {
+    this._tableViewCompact = false;
+    this.bindEvents();
   };
 
   _.extend( DataTableView.prototype, {
+    bindEvents: function() {
+      $(".c-change-table-view").on( "click", _.bind( this.onChangeTableView, this ) );
+    },
+
     showQueryResults: function( qr ) {
       var aspects = this.preferences().aspects();
       var data = this.formulateData( qr, aspects );
+      var columns = this.formulateColumns( aspects );
 
       googleAnalyticsDataTablesWorkaround();
 
       $(".c-results").removeClass( "js-hidden" );
+
+      // this should be necessary, but the destroy:true attrib on DataTables is not working
+      if ($("#results-table.dataTable").length > 0) {
+        $("#results-table").DataTable().destroy( false );
+        $("#results-table").children().remove();
+      }
+
       $("#results-table").DataTable( {
         data: data,
-        destroy: true,  // todo there are more efficient ways than this
-        columns: [
-          {title: "Date"},
-          {title: "Measure"},
-          {title: "Value", type: "num-fmt", className: "text-right"}
-        ]
+        columns: columns,
+        searching: false
       } );
     },
 
@@ -40,6 +52,15 @@ modulejs.define( "data-table-view", [
     },
 
     formulateData: function( qr, aspects ) {
+      if (this._tableViewCompact) {
+        return this.formulateDataWide( qr, aspects );
+      }
+      else {
+        return this.formulateDataNarrow( qr, aspects );
+      }
+    },
+
+    formulateDataNarrow: function( qr, aspects ) {
       var rowSets = _.map( qr.results(), function( result ) {
         var values = result.valuesFor( aspects );
         var zValues = _.zip( aspects, values );
@@ -47,13 +68,59 @@ modulejs.define( "data-table-view", [
         return _.map( zValues, function( zv ) {
           return [
             result.period(),
-            zv[0],
+            formatAspectNameLong( zv[0], " " ),
             zv[1] ? formatValue( zv[0], zv[1] ) : null
           ];
         } );
       } );
 
       return _.flatten( rowSets );
+    },
+
+    formulateDataWide: function( qr, aspects ) {
+      return _.map( qr.results(), function( result ) {
+        var values = result.valuesFor( aspects );
+        var formattedValues = _.map( _.zip( aspects, values ), function( pair ) {
+          return formatValue.apply( null, pair );
+        } );
+
+        return [result.period()].concat( formattedValues );
+      } );
+    },
+
+    formulateColumns: function( aspects ) {
+      return this._tableViewCompact ? this.formulateColumnsWide( aspects ) : this.formulateColumnsNarrow();
+    },
+
+    formulateColumnsNarrow: function() {
+      return [
+        {title: "Date"},
+        {title: "Measure"},
+        {title: "Value", type: "num-fmt", className: "text-right"}
+      ];
+    },
+
+    formulateColumnsWide: function( aspects ) {
+      var aspectColumns = _.map( aspects, function( aspect ) {
+        return {title: formatAspectNameShort( aspect ), className: "text-right", type: "num-fmt"};
+      } );
+
+      return [{title: "Date"}].concat( aspectColumns );
+    },
+
+    // event handlers
+
+    onChangeTableView: function() {
+      this._tableViewCompact = !this._tableViewCompact;
+
+      var buttonLabel = this._tableViewCompact ?
+        "Return to narrow table view" :
+        "Switch to compact table view";
+      $(".c-change-table-view span").text( buttonLabel );
+
+      $(".c-results-table-container").toggleClass( "c-results-table--compact", this._tableViewCompact );
+
+      $("body").trigger( Constants.EVENT_PREFERENCES_CHANGE );
     }
 
   } );
@@ -66,8 +133,68 @@ modulejs.define( "data-table-view", [
       case "pound_sterling":
         return "£" + value;
       default:
-        return value;
+        return _.isFinite( value ) ? value.toFixed( 2 ) : value;
     }
+  };
+
+  var formatAspectNameShort = function( aspectName, sep ) {
+    sep = sep || "<br />";
+
+    var shortNames = {
+      averagePrice: "avg. price (all)",
+      housePriceIndex: "index (all)",
+      housePriceIndexSA: "index-SA (all)",
+      percentageChange: "change (all)",
+      percentageAnnualChange: "annual change (all)",
+      averagePriceSA: "avg. price-SA (all)",
+      salesVolume: "sales volume",
+      averagePriceDetached: "avg. price (det.)",
+      housePriceIndexDetached: "index (det.)",
+      percentageChangeDetached: "change (det.)",
+      percentageAnnualChangeDetached: "annual change (det.)",
+      averagePriceSemiDetached: "avg. price (semi-det.)",
+      housePriceIndexSemiDetached: "index (semi-det.)",
+      percentageChangeSemiDetached: "change (semi-det.)",
+      percentageAnnualChangeSemiDetached: "annual change (semi-det.)",
+      averagePriceTerraced: "avg. price (terr.)",
+      housePriceIndexTerraced: "index (terr.)",
+      percentageChangeTerraced: "change (terr.)",
+      percentageAnnualChangeTerraced: "annual change (terr.)",
+      averagePriceFlatMaisonette: "avg price (flat/mais.)",
+      housePriceIndexFlatMaisonette: "index (flat/mais.)",
+      percentageChangeFlatMaisonette: "change (flat/mais.)",
+      percentageAnnualChangeFlatMaisonette: "yearly change (flat/mais.)",
+      averagePriceCash: "avg. price (cash)",
+      housePriceIndexCash: "index (cash)",
+      percentageChangeCash: "change (cash)",
+      percentageAnnualChangeCash: "yearly change (cash)",
+      averagePriceMortgage: "avg. price (mtge)",
+      housePriceIndexMortgage: "index (mtge)",
+      percentageChangeMortgage: "change (mtge)",
+      percentageAnnualChangeMortgage: "yearly change (mtge)",
+      averagePriceFirstTimeBuyer: "avg. price (ftb)",
+      housePriceIndexFirstTimeBuyer: "index (ftb)",
+      percentageChangeFirstTimeBuyer: "change (ftb)",
+      percentageAnnualChangeFirstTimeBuyer: "yearly change (ftb)",
+      averagePriceFormerOwnerOccupier: "avg. price (foo)",
+      housePriceIndexFormerOwnerOccupier: "index (foo)",
+      percentageChangeFormerOwnerOccupier: "change (foo)",
+      percentageAnnualChangeFormerOwnerOccupier: "yearly change (foo)",
+      averagePriceNewBuild: "avg. price (new)",
+      housePriceIndexNewBuild: "index (new)",
+      percentageChangeNewBuild: "change (new)",
+      percentageAnnualChangeNewBuild: "yearly change (new)",
+      averagePriceExistingProperty: "avg. price (existing)",
+      housePriceIndexExistingProperty: "index (existing)",
+      percentageChangeExistingProperty: "change (existing)",
+      percentageAnnualChangeExistingProperty: "yearly change (existing)",
+    };
+
+    return shortNames[aspectName].replace( / /g, sep );
+  };
+
+  var formatAspectNameLong = function( aspectName ) {
+    return Aspects[aspectName].label;
   };
 
   /** This bug is caused by GA side-effecting String.prototype */
