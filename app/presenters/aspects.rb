ee1b34415c00c26542497e2fec7a3419097d37f2
Ian Dickinson <i.j.dickinson@gmail.com>
2017-08-16 16:43:18 +0100

    Eliminating redundant code

--- app/presenters/aspects.rb
+++ app/presenters/aspects.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-Struct.new('AspectGroup', :label, :"advanced?", :measures)
-
 # Presenter for managing which measures of the cube are presented as aspects,
 # according to the current user preferences
 class Aspects
