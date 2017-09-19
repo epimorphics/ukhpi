a6888d6a6e7f812bc1aa21579ce5272d0384abfe
Ian Dickinson <i.j.dickinson@gmail.com>
2017-09-19 17:31:50 +0100

    Refactoring: extrace CubeMeasure to stand-alone class

---
+++ app/models/concerns/cube_data_model/cube_measure.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module CubeDataModel
+  # Specialises a CubeResource to add behaviour specific to DataCube measures
+  class CubeMeasure < CubeResource
+    include CubeDataModel::Vocabularies
+
+    def units
+      objects_of(QUDT.unit)
+    end
+
+    def range
+      objects_of(RDF::RDFS.range)
+    end
+
+    def scalar?
+      units.include?(DBPEDIA_RESOURCE.Scalar)
+    end
+
+    def percentage?
+      units.include?(DBPEDIA_RESOURCE.Percentage)
+    end
+
+    def pound_sterling?
+      units.include?(DBPEDIA_RESOURCE.Pound_sterling)
+    end
+
+    def integer_range?
+      range.include?(RDF::XSD.integer)
+    end
+
+    def decimal_range?
+      range.include?(RDF::XSD.decimal)
+    end
+
+    def unit_type # rubocop:disable Metrics/MethodLength
+      if scalar?
+        :scalar
+      elsif percentage?
+        :percentage
+      elsif pound_sterling?
+        :pound_sterling
+      elsif integer_range?
+        :integer
+      elsif decimal_range?
+        :decimal
+      else
+        :unknown
+      end
+    end
+  end
+end
---
+++ test/models/concerns/cube_data_model/cube_measure_test.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# :nodoc:
+class CubeMeasureTest < ActiveSupport::TestCase
+  let :measure_0 do
+    g = RDF::Graph.new
+    measure = RDF::Resource.new('http://landregistry.data.gov.uk/def/ukhpi/salesVolume')
+
+    g.from_ttl <<~GRAPH
+      @prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .
+      @prefix owl:   <http://www.w3.org/2002/07/owl#> .
+      @prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .
+      @prefix api:   <http://purl.org/linked-data/api/vocab#> .
+      @prefix def-ukhpi: <http://landregistry.data.gov.uk/def/ukhpi/> .
+      @prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+      @prefix qb:    <http://purl.org/linked-data/cube#> .
+      @prefix dcterms: <http://purl.org/dc/terms/> .
+      def-ukhpi:salesVolume
+            a             owl:DatatypeProperty , qb:MeasureProperty ;
+            rdfs:comment  "Volume of sales upon which analysis is based"@en ;
+            rdfs:label    "Sales volume"@en ;
+            rdfs:range    xsd:integer ;
+            <http://qudt.org/schema/qudt#unit>  <http://dbpedia.org/resource/Number> .
+    GRAPH
+
+    CubeDataModel::CubeMeasure.new(g, measure)
+  end
+
+  let :measure_1 do
+    g = RDF::Graph.new
+    measure = RDF::Resource.new('http://landregistry.data.gov.uk/def/ukhpi/percentageChangeDetached')
+
+    g.from_ttl <<~GRAPH
+      @prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .
+      @prefix owl:   <http://www.w3.org/2002/07/owl#> .
+      @prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .
+      @prefix api:   <http://purl.org/linked-data/api/vocab#> .
+      @prefix def-ukhpi: <http://landregistry.data.gov.uk/def/ukhpi/> .
+      @prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+      @prefix qb:    <http://purl.org/linked-data/cube#> .
+      @prefix dcterms: <http://purl.org/dc/terms/> .
+      def-ukhpi:percentageChangeDetached
+        a             owl:DatatypeProperty , qb:MeasureProperty ;
+        rdfs:comment  "The percentage change for the average detached property price compared to the previous reference period, aggregated over the given reference period."@en ;
+        rdfs:label    "Detached property price percentage change"@en ;
+        rdfs:range    xsd:decimal ;
+        <http://qudt.org/schema/qudt#unit>  <http://dbpedia.org/resource/Percentage> .
+    GRAPH
+
+    CubeDataModel::CubeMeasure.new(g, measure)
+  end
+
+  describe 'CubeMeasure' do
+    it 'should return the units' do
+      measure_0.units.map(&:to_s).must_equal ['http://dbpedia.org/resource/Number']
+    end
+
+    it 'should return the range' do
+      measure_0.range.map(&:to_s).must_equal [RDF::XSD.integer.to_s]
+    end
+
+    it 'should return true for a percentage measure' do
+      measure_0.percentage?.must_equal false
+      measure_1.percentage?.must_equal true
+    end
+
+    it 'should return the unit type' do
+      measure_0.unit_type.must_equal :integer
+      measure_1.unit_type.must_equal :percentage
+    end
+  end
+end
