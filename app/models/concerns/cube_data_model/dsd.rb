098bc5ee95e582e35eccc773e359b73199de69e9
Ian Dickinson <i.j.dickinson@gmail.com>
2017-09-19 17:48:44 +0100

    Refactoring: extract DSD encapsulation to stand-alone class

---
+++ app/models/concerns/cube_data_model/dsd.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module CubeDataModel
+  # Encapsulates a DataCube DSD
+  class DSD
+    include CubeDataModel::Vocabularies
+
+    attr_reader :graph
+    attr_reader :root
+
+    def initialize(graph, root)
+      @graph = graph
+      @root = root
+    end
+
+    def components
+      graph
+        .query([root, QB.component, nil])
+        .map { |stmt| CubeComponent.new(graph, stmt.object) }
+    end
+
+    def measures
+      components
+        .select(&:"measure?")
+        .map(&:measure)
+    end
+
+    def dimensions
+      components
+        .select(&:"dimension?")
+        .map(&:dimension)
+    end
+  end
+end
---
+++ test/models/concerns/cube_data_model/dsd_test.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# Unit tests on the DSD class
+class DsdTest < ActiveSupport::TestCase
+  let :dsd do
+    g = RDF::Graph.new
+
+    g.from_ttl <<~GRAPH
+      @prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .
+      @prefix owl:   <http://www.w3.org/2002/07/owl#> .
+      @prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .
+      @prefix api:   <http://purl.org/linked-data/api/vocab#> .
+      @prefix def-ukhpi: <http://landregistry.data.gov.uk/def/ukhpi/> .
+      @prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+      @prefix qb:    <http://purl.org/linked-data/cube#> .
+      @prefix dcterms: <http://purl.org/dc/terms/> .
+
+      def-ukhpi:datasetDefinition
+        a             qb:DataStructureDefinition ;
+        qb:component  [ qb:dimension  def-ukhpi:refRegion ] ;
+        qb:component  [ qb:measure  def-ukhpi:averagePrice ] ;
+        .
+
+        def-ukhpi:refRegion  a  owl:ObjectProperty , qb:DimensionProperty ;
+          rdfs:comment  "The administrative areas that Land Registry"@en ;
+          rdfs:label    "Region"@en ;
+          rdfs:range    def-ukhpi:Region .
+
+          def-ukhpi:averagePrice
+            a             owl:DatatypeProperty , qb:MeasureProperty ;
+            rdfs:comment  "Average price at Country, Regional, County/Unitary/District Authority"@en ;
+            rdfs:label    "Average price"@en ;
+            rdfs:range    xsd:integer ;
+            <http://qudt.org/schema/qudt#unit>
+            <http://dbpedia.org/resource/Pound_sterling> .
+    GRAPH
+
+    CubeDataModel::DSD.new(g, CubeDataModel::Vocabularies::UKHPI.datasetDefinition)
+  end
+
+  it 'should return a collection of components' do
+    dsd.components.length.must_equal 2
+  end
+
+  it 'should return a collection of only dimensions' do
+    dsd.dimensions.length.must_equal 1
+    dsd.dimensions.first.uri.must_equal 'http://landregistry.data.gov.uk/def/ukhpi/refRegion'
+  end
+
+  it 'should return a collection of only measures' do
+    dsd.measures.length.must_equal 1
+    dsd.measures.first.uri.must_equal 'http://landregistry.data.gov.uk/def/ukhpi/averagePrice'
+  end
+end
