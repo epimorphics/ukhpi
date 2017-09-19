f55dfc3044ff8ba24f2c6d9bfda4f64a5c3784db
Ian Dickinson <i.j.dickinson@gmail.com>
2017-09-19 11:45:21 +0100

    Refactoring: extract CubeResource to includable module

---
+++ app/models/concerns/cube_data_model/cube_resource.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module CubeDataModel
+  # Concern that encapsulates an RDF resource in a DataCube model
+  module CubeResource
+    attr_reader :resource, :graph
+
+    def label
+      graph.query([resource, RDF::RDFS.label, nil]).first.object.to_s
+    end
+
+    def comment
+      graph.query([resource, RDF::RDFS.comment, nil]).first.object.to_s
+    end
+
+    def range
+      graph.query([resource, RDF::RDFS.range, nil]).first.object
+    end
+
+    def slug
+      slug_mixed_case = local_name[0] + local_name.gsub(/[[:lower:]]/, '')
+      slug_mixed_case.downcase
+    end
+
+    def local_name
+      resource.to_s.match(%r{([^/\#]*)\Z})[1]
+    end
+
+    def uri
+      resource.to_s
+    end
+
+    def qname
+      "ukhpi:#{local_name}"
+    end
+  end
+end
---
+++ test/models/concerns/cube_data_model/cube_resource_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CubeResourceTestable
+  include CubeDataModel::CubeResource
+
+  def initialize(graph, resource)
+    @graph = graph
+    @resource = resource
+  end
+end
+
+# Unit tests on the DataModel class and helpers
+class CubeResourceTest < ActiveSupport::TestCase
+  let :resource do
+    g = RDF::Graph.new
+    r = RDF::Resource.new('http://test.test/testResource')
+    q = RDF::Resource.new('http://test.test/q')
+
+    g << RDF::Statement(r, RDF::RDFS.label, 'I am Groot')
+    g << RDF::Statement(r, RDF::RDFS.comment, 'watch this!')
+    g << RDF::Statement(r, RDF::RDFS.range, q)
+
+    CubeResourceTestable.new(g, r)
+  end
+
+  describe 'CubeResource' do
+    it 'should return the label' do
+      resource.label.must_equal 'I am Groot'
+    end
+
+    it 'should return the comment' do
+      resource.comment.must_equal 'watch this!'
+    end
+
+    it 'should return the range of the resource' do
+      resource.range.to_s.must_equal 'http://test.test/q'
+    end
+
+    it 'should calculate a slug for the resource' do
+      resource.slug.must_equal 'tr'
+    end
+
+    it 'should extract the local name' do
+      resource.local_name.must_equal 'testResource'
+    end
+
+    it 'should return the URI' do
+      resource.uri.must_equal 'http://test.test/testResource'
+    end
+
+    it 'should return the qname' do
+      resource.qname.must_equal 'ukhpi:testResource'
+    end
+  end
+end
