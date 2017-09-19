91121845c1640f31de448cb9b26b07d9b0c07858
Ian Dickinson <i.j.dickinson@gmail.com>
2017-09-19 18:04:27 +0100

    Refactoring: UkhpiDataCube now uses refactored CubeDataModel classes. Tests pass

--- app/models/ukhpi_data_cube.rb
+++ app/models/ukhpi_data_cube.rb
@@ -1,157 +1,27 @@
 # frozen_string_literal: true
 
-# :nodoc:
-class CubeResource
-  attr_reader :resource, :graph
-
-  def initialize(graph, resource)
-    @graph = graph
-    @resource = resource
-  end
-
-  def label
-    @graph.query([@resource, RDF::RDFS.label, nil]).first.object.to_s
-  end
-
-  def comment
-    @graph.query([@resource, RDF::RDFS.comment, nil]).first.object.to_s
-  end
-
-  def range
-    @graph.query([@resource, RDF::RDFS.range, nil]).first.object
-  end
-
-  def slug
-    slug_mixed_case = local_name[0] + local_name.gsub(/[[:lower:]]/, '')
-    slug_mixed_case.downcase
-  end
-
-  def local_name
-    @resource.to_s.match(%r{([^/\#]*)\Z})[1]
-  end
-
-  def uri
-    @resource.to_s
-  end
-
-  def qname
-    "ukhpi:#{local_name}"
-  end
-end
-
-# :nodoc:
-class CubeComponent < CubeResource
-  def dimension?
-    !@graph.query([@resource, DataModel::QB.dimension, nil]).empty?
-  end
-
-  def measure?
-    !@graph.query([@resource, DataModel::QB.measure, nil]).empty?
-  end
-
-  def measure
-    r = @graph.query([@resource, DataModel::QB.measure, nil]).first.object
-    CubeMeasure.new(@graph, r)
-  end
-
-  def dimension
-    r = @graph.query([@resource, DataModel::QB.dimension, nil]).first.object
-    CubeDimension.new(@graph, r)
-  end
-end
-
-# :nodoc:
-class CubeMeasure < CubeResource
-  def unit
-    @graph.query([@resource, DataModel::QUDT.unit, nil]).first.object
-  end
-
-  def range
-    @graph.query([@resource, RDF::RDFS.range, nil]).first.object
-  end
-
-  def scalar?
-    unit == RDF::Resource.new('http://dbpedia.org/page/Scalar')
-  end
-
-  def percentage?
-    unit == RDF::Resource.new('http://dbpedia.org/resource/Percentage')
-  end
-
-  def pound_sterling?
-    unit == RDF::Resource.new('http://dbpedia.org/resource/Pound_sterling')
-  end
-
-  def integer_range?
-    range == RDF::XSD.integer
-  end
-
-  def decimal_range?
-    range == RDF::XSD.decimal
-  end
-
-  def unit_type # rubocop:disable Metrics/MethodLength
-    if scalar?
-      :scalar
-    elsif percentage?
-      :percentage
-    elsif pound_sterling?
-      :pound_sterling
-    elsif integer_range?
-      :integer
-    elsif decimal_range?
-      :decimal
-    else
-      :unknown
-    end
-  end
-end
-
-# :nodoc:
-class CubeDimension < CubeResource
-end
-
 # An encapsulation of the DSD denoting the RDF cube data model for UKHPI
-class DataModel
-  # RDF vocabularies
-  QB = RDF::Vocabulary.new('http://purl.org/linked-data/cube#')
-  UKHPI = RDF::Vocabulary.new('http://landregistry.data.gov.uk/def/ukhpi/')
-  QUDT = RDF::Vocabulary.new('http://qudt.org/schema/qudt#')
+class UkhpiDataCube
+  include CubeDataModel::Vocabularies
+
+  CONFIG_DIR = 'dsapi'
+  DSD_FILE = 'UKHPI-dsd.ttl'
+
+  attr_reader :dsd
 
   def initialize
-    load_model
-  end
-
-  def model
-    @@model
-  end
-
-  def components
-    model
-      .query([UKHPI.datasetDefinition, QB.component, nil])
-      .map { |stmt| CubeComponent.new(model, stmt.object) }
-  end
-
-  def measures
-    components
-      .select(&:"measure?")
-      .map(&:measure)
-  end
-
-  def dimensions
-    components
-      .select(&:"dimension?")
-      .map(&:dimension)
+    @dsd = CubeDataModel::DSD.new(load_model, UKHPI.datasetDefinition)
   end
 
   private
 
   def load_model
     read_data_model unless defined?(@@model)
+    @@model
   end
 
   def read_data_model
-    file = File.join(Rails.root, 'config', 'dsapi', 'UKHPI-dsd.ttl')
+    file = File.join(Rails.root, 'config', CONFIG_DIR, DSD_FILE)
     @@model = RDF::Graph.load(file, format: :ttl) # rubocop:disable Style/ClassVars
   end
 end
--- test/models/ukhpi_data_cube_test.rb
+++ test/models/ukhpi_data_cube_test.rb
@@ -5,48 +5,42 @@
 require 'test_helper'
 
 class DataModelTest < ActiveSupport::TestCase
+  let :cube { UkhpiDataCube.new }
+
   it 'should load the DSD' do
-    dsd = DataModel.new
-    dsd.model.wont_be_nil
+    cube.dsd.graph.wont_be_nil
   end
 
   it 'should list the components' do
-    dsd = DataModel.new
-    dsd.components.count.must_be :>=, 49
+    cube.dsd.components.count.must_be :>=, 49
   end
 
   it 'should retrieve the labels for each compoonent' do
-    dsd = DataModel.new
-    labels = dsd.measures.map(&:label)
+    labels = cube.dsd.measures.map(&:label)
     labels.must_include 'Average price'
   end
 
   it 'should retrieve the comments for each component' do
-    dsd = DataModel.new
-    comments = dsd.measures.map(&:comment)
+    comments = cube.dsd.measures.map(&:comment)
     comments.must_include('The percentage change in the average house price compared to the same period twelve months earlier.') # rubocop:disable Metrics/LineLength
   end
 
   it 'should separate out the dimensions' do
-    dsd = DataModel.new
-    dsd.dimensions.count.must_equal 2
+    cube.dsd.dimensions.count.must_equal 2
   end
 
   it 'should separate out the measures' do
-    dsd = DataModel.new
-    dsd.measures.count.must_equal 47
+    cube.dsd.measures.count.must_be :>=, 47
   end
 
   it 'should produce a unique slug for each measure' do
-    dsd = DataModel.new
-    slugs = dsd.measures.map(&:slug)
+    slugs = cube.dsd.measures.map(&:slug)
     slugs.must_include 'apsd'
     slugs.uniq.length.must_equal slugs.length
   end
 
   it 'should produce a qname for each measure' do
-    dsd = DataModel.new
-    slugs = dsd.measures.map(&:qname)
+    slugs = cube.dsd.measures.map(&:qname)
     slugs.must_include 'ukhpi:averagePriceSemiDetached'
     slugs.uniq.length.must_equal slugs.length
   end
