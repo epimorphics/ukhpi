366c08b326238b461f2b4c2030144cfae784dc82
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-09-20 17:28:26 +0100

    Add knowledge about the regions we can search for and their types

--- app/models/regions.rb
+++ app/models/regions.rb
@@ -6,6 +6,28 @@ require 'regions-table'
 class Regions
   extend RegionsTable
 
+  Struct.new('LocationSearchType', :label, :rdf_types)
+
+  # rubocop:disable Layout/IndentArray
+  LOCATION_SEARCH_TYPES = {
+    country: Struct::LocationSearchType.new('Country', [
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion'
+    ]),
+    local_authority: Struct::LocationSearchType.new('Local authority', [
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/Borough',
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/District',
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/LondonBorough',
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/MetropolitanDistrict',
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/UnitaryAuthority'
+    ]),
+    england_region: Struct::LocationSearchType.new('Region (England only)', [
+      'http://landregistry.data.gov.uk/def/ukhpi/Region'
+    ]),
+    england_county: Struct::LocationSearchType.new('County (England only)', [
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/County'
+    ])
+  }.freeze
+
   def self.match(region_name, params)
     rtype = validate_region_type(params[:rt])
     locations.select do |_uri, loc|
@@ -30,5 +52,13 @@ class Regions
 
   def self.each_location(&block)
     locations.values.each(&block)
+  end
+
+  # @return A list of options for the types of location search, in a form suitable
+  # for building a select element in the UI
+  def self.location_search_options
+    LOCATION_SEARCH_TYPES.map do |key, search_option|
+      [search_option.label, key]
+    end
   end
 end
