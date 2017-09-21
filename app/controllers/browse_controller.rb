17cf96e2061a68f71a9fbb1ffae67924bb78390c
Ian Dickinson <i.j.dickinson@gmail.com>
2017-09-21 15:43:15 +0100

    Basic interaction pattern for searching for a location and selecting among choices, non-JavaScript

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -9,8 +9,56 @@ class BrowseController < ApplicationController
 
   def edit
     @view_state = BrowseViewState.new(params)
+    process_form_action(@view_state)
   end
 
   # private
 
+  # Look at the `action` parameter, which may be set by various action buttons
+  # on the form, to determine whether we need to do any processing before
+  # dislaying the form
+  def process_form_action(view_state)
+    action = view_state.user_selections.action
+
+    case action&.downcase
+    when 'search'
+      search_for_location(view_state)
+    end
+  end
+
+  # User clicked the search button
+  def search_for_location(view_state)
+    if !view_state.location_search_term&.empty?
+      match_location(view_state)
+    else
+      flash[:search] = 'Missing search term'
+    end
+  end
+
+  def match_location(view_state)
+    locations = Locations.search_location(view_state.location_search_term,
+                                          view_state.location_search_type)
+
+    case locations.length
+    when 0
+      match_no_locations
+    when 1
+      match_single_location(view_state, locations)
+    else
+      match_multiple_locations(view_state, locations)
+    end
+  end
+
+  def match_no_locations
+    flash[:search] = 'Sorry, no locations match that search term'
+  end
+
+  def match_single_location(view_state, locations)
+    view_state.location = locations.first
+  end
+
+  def match_multiple_locations(view_state, locations)
+    view_state.add_matched_locations(locations)
+    view_state.user_selections.clear_selected_location
+  end
 end
--- app/presenters/browse_view_state.rb
+++ app/presenters/browse_view_state.rb
@@ -3,6 +3,7 @@
 # Adapter mapping the view state in a browse controller to the browse view
 class BrowseViewState
   attr_reader :user_selections
+  attr_reader :matched_locations
 
   def initialize(params)
     @user_selections = UserSelections.new(params)
@@ -10,6 +11,46 @@ class BrowseViewState
 
   def location_type_options
     Regions.location_search_options
+  end
+
+  def selected_location
+    user_selections.selected_location
+  end
+
+  def selected_location_label
+    loc = selected_location
+    loc && Regions.region_label(loc)
+  end
+
+  def location_search_term
+    user_selections.location_search_term
+  end
+
+  def location_search_type
+    user_selections.location_search_type
+  end
+
+  def add_matched_locations(locations)
+    @matched_locations = locations
+  end
+
+  def location=(location)
+    @user_selections =
+      user_selections
+      .with('location', location.uri)
+      .without('location-term')
+      .without('location-type')
+      .without('form-action')
+  end
+
+  def with_location_path(location)
+    selection_with_loc =
+      user_selections
+      .without('location-term')
+      .without('location-type')
+      .without('form-action')
+      .with('location', location.uri)
+    "?#{UserSelectionsPresenter.new(selection_with_loc).as_url_search_string}"
   end
 
   private
