704e809d398c9636ab0094676ed8f9ee38ce5aaf
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-09-25 15:34:49 +0100

    Some basic refactoring of QueryCommand. Now builds DsAPI expressoin on #initialize. Updated tests.

--- app/services/query_command.rb
+++ app/services/query_command.rb
@@ -4,51 +4,66 @@
 class QueryCommand
   include DataService
 
-  attr_reader :prefs, :results
+  MILLISECONDS = 1000.0
 
-  def initialize(prefs)
-    @prefs = prefs
+  attr_reader :user_selections
+  attr_reader :results
+  attr_reader :query
+
+  def initialize(user_selections)
+    @user_selections = user_selections
+    @query = build_query
   end
 
-  def perform_query(service = nil) # rubocop:disable Metrics/AbcSize
-    hpi = service || dataset(:ukhpi)
-
-    query = add_date_range_constraint(base_query)
-    query = add_location_constraint(query)
-
+  # Perform the UKHPI query encapsulated by this command object
+  # @param service Optional API service end-point to use. Defaults to the UKHPI
+  # API service endpoint
+  def perform_query(service = nil)
     Rails.logger.debug "About to ask DsAPI query: #{query.to_json}"
     Rails.logger.debug query.to_json
-    start = Time.now
-    @results = hpi.query(query)
-    Rails.logger.debug(format("query took %.1f ms\n", ((Time.now - start) * 1000.0)))
+    time_taken = execute_query(service, query)
+    Rails.logger.debug(format("query took %.1f ms\n", time_taken))
   end
 
+  # @return True if this a query execution command
   def query_command?
     true
   end
 
+  # @return True if this is a query explanation command
   def explain_query_command?
     false
   end
 
   private
 
-  def preference(key)
-    prefs.send(key)
+  # Construct the DsAPI query that matches the given user constraints
+  def build_query
+    query = add_date_range_constraint(base_query)
+    add_location_constraint(query)
+  end
+
+  def api_service(service)
+    @api_service ||= service || default_service
+  end
+
+  def default_service
+    dataset(:ukhpi)
+  end
+
+  # Run the given query, and stash the results. Return time taken in ms.
+  def execute_query(service, query)
+    start = Time.now
+    @results = api_service(service).query(query)
+    (Time.now - start) * MILLISECONDS
   end
 
   def add_date_range_constraint(query)
-    query.ge('ukhpi:refMonth', month_year_value(:from))
-         .le('ukhpi:refMonth', month_year_value(:to))
-  end
+    from = month_year_value(user_selections.from_date)
+    to = month_year_value(user_selections.to_date)
 
-  def month_year_value(key)
-    date = boundary_date(key)
-    DataServicesApi::Value.year_month(date.year, date.month)
-  end
-
-  def boundary_date(key)
-    preference(key)
+    query.ge('ukhpi:refMonth', from)
+         .le('ukhpi:refMonth', to)
   end
 
   def add_location_constraint(query)
@@ -56,7 +71,11 @@ class QueryCommand
     query.eq('ukhpi:refRegion', value)
   end
 
+  def month_year_value(date)
+    DataServicesApi::Value.year_month(date.year, date.month)
+  end
+
   def region_uri
-    preference(:region_uri)
+    user_selections.selected_region
   end
 end
--- test/services/query_command_test.rb
+++ test/services/query_command_test.rb
@@ -1,47 +1,66 @@
 # frozen_string_literal: true
 
-# Unit tests on the QueryCommand class
-
 require 'test_helper'
 
 class MockService
-  attr_reader :capture
+  attr_reader :captured
 
   def query(query)
-    @capture = query.to_json
+    @captured = query.to_json
   end
 end
 
+def validate_json(json) # rubocop:disable Metrics/MethodLength
+  json.must_match_json_expression(
+    "@and":
+    [
+      { "ukhpi:refMonth": {
+        "@ge": { "@value": '2015-01', "@type": 'http://www.w3.org/2001/XMLSchema#gYearMonth' }
+      } },
+      { "ukhpi:refMonth": {
+        "@le": { "@value": '2016-06', "@type": 'http://www.w3.org/2001/XMLSchema#gYearMonth' }
+      } },
+      { "ukhpi:refRegion": {
+        "@eq": { "@id": 'http://fubar.com/foo' }
+      } }
+    ]
+  )
+end
+
+# Unit tests on the QueryCommand class
 class QueryCommandTest < ActiveSupport::TestCase
-  it 'constructs a query correctly' do
-    service = MockService.new
-    sc = mock
-    sc.expects(:from).returns(Date.new(2015, 1, 1))
-    sc.expects(:to).returns(Date.new(2016, 6, 1))
-    sc.expects(:region_uri).returns('http://fubar.com/foo')
-
-    qc = QueryCommand.new(sc)
-    qc.perform_query(service)
-
-    json = service.capture
-    json.wont_be_nil
-    json.must_match_json_expression(
-      "@and":
-        [
-          { "ukhpi:refMonth": {
-            "@ge": { "@value": '2015-01', "@type": 'http://www.w3.org/2001/XMLSchema#gYearMonth' }
-          } },
-          { "ukhpi:refMonth": {
-            "@le": { "@value": '2016-06', "@type": 'http://www.w3.org/2001/XMLSchema#gYearMonth' }
-          } },
-          { "ukhpi:refRegion": {
-            "@eq": { "@id": 'http://fubar.com/foo' }
-          } }
-        ]
-    )
+  let :user_selections do
+    user_selections = mock
+    user_selections.expects(:from_date).returns(Date.new(2015, 1, 1))
+    user_selections.expects(:to_date).returns(Date.new(2016, 6, 1))
+    user_selections.expects(:selected_region).returns('http://fubar.com/foo')
+    user_selections
   end
 
-  it 'should state that it is a query command' do
-    QueryCommand.new({}).query_command?.must_equal true
+  let :qc { QueryCommand.new(user_selections) }
+
+  describe 'QueryCommand' do
+    describe '#initialize' do
+      it 'constructs a query correctly' do
+        json = qc.query.to_json
+        json.wont_be_nil
+        validate_json(json)
+      end
+    end
+
+    describe '#perform_query' do
+      it 'calls the service endpoint correctly' do
+        mock_service = MockService.new
+        qc.perform_query(mock_service)
+        validate_json(mock_service.captured)
+      end
+    end
+  end
+
+  describe '#query_command?' do
+    it 'should report its type correctly' do
+      assert qc.query_command?
+      refute qc.explain_query_command?
+    end
   end
 end
