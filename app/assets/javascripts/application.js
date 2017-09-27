8eb0d47b9982fea15b320f37f91f47762eb9a3eb
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-09-27 10:39:32 +0100

    Temporarily disable some JS loading during transition from asset pipeline to webpack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,37 +1,37 @@
-// = require jquery
-// = require lodash
-// = require modulejs
-// = require bootstrap
-// = require dataTables/jquery.dataTables
-// = require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-// = require dataTables.buttons.min
-// = require buttons.print.min
-// = require moment
-// = require bootstrap-datetimepicker
-// = require bootstrap3-typeahead
-// = require leaflet
-// = require spin
-// = require jquery.spin
-// = require_tree .
+// = r e q u i r e jquery
+// = r e q u i r e lodash
+// = r e q u i r e modulejs
+// = r e q u i r e bootstrap
+// = r e q u i r e dataTables/jquery.dataTables
+// = r e q u i r e dataTables/bootstrap/3/jquery.dataTables.bootstrap
+// = r e q u i r e dataTables.buttons.min
+// = r e q u i r e buttons.print.min
+// = r e q u i r e moment
+// = r e q u i r e bootstrap-datetimepicker
+// = r e q u i r e bootstrap3-typeahead
+// = r e q u i r e leaflet
+// = r e q u i r e spin
+// = r e q u i r e jquery.spin
+// = r e q u i r e_tree .
 
-modulejs.define( "application", [
-  "router"
-],
-function(
-  Router
-) {
-  "use strict";
-
-  var router = new Router();
-  router.invoke( window.location );
-
-  return {
-    router: router
-  };
-} );
-
-/* global $ */
-$( function() {
-  "use strict";
-  modulejs.require( "application" );
-} );
+// modulejs.define( "application", [
+//   "router"
+// ],
+// function(
+//   Router
+// ) {
+//   "use strict";
+//
+//   var router = new Router();
+//   router.invoke( window.location );
+//
+//   return {
+//     router: router
+//   };
+// } );
+//
+// /* global $ */
+// $( function() {
+//   "use strict";
+//   modulejs.require( "application" );
+// } );
