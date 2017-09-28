1aaa87e9766f12915b49f4599b9455bcc180ba23
Ian Dickinson <i.j.dickinson@gmail.com>
2017-09-28 15:21:36 +0100

    Mount a new Vue app for each vue-root element

--- app/javascript/packs/ukhpi_vue.js
+++ app/javascript/packs/ukhpi_vue.js
@@ -1,15 +1,11 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> and
-// <%= stylesheet_pack_tag 'hello_vue' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
-import Vue from 'vue'
-import App from './app.vue'
+import Vue from 'vue';
+import App from './app.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('hello'))
-  const app = new Vue(App).$mount('hello')
+  const nodes = document.querySelectorAll('.o-data-view__vue-root');
 
-  console.log(app)
-})
+  for (let i = 0; i < nodes.length; i += 1) {
+    const node = nodes.item(i);
+    new Vue(App).$mount(node);
+  }
+});
