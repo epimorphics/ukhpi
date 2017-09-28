ef5929462341bf1863f53742b793482b18eee889
Ian Dickinson <i.j.dickinson@gmail.com>
2017-09-28 16:48:38 +0100

    Change root component from App to DataView

---
+++ app/javascript/packs/data-view.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class='o-data-view__vue-root u-js-only'>
+    We are devo
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss">
+</style>
--- app/javascript/packs/ukhpi_vue.js
+++ app/javascript/packs/ukhpi_vue.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import App from './app.vue';
+import DataView from './data-view.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const nodes = document.querySelectorAll('.o-data-view__vue-root');
 
   for (let i = 0; i < nodes.length; i += 1) {
     const node = nodes.item(i);
-    new Vue(App).$mount(node);
+    new Vue(DataView).$mount(node);
   }
 });
