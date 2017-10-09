67a5aff590b2da471861090182e922afa9329228
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-10-09 11:16:38 +0100

    Ensure we precompile font assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,4 +16,4 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets')
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w[.svg']
+Rails.application.config.assets.precompile += %w[.svg .eot .woff .ttf]
