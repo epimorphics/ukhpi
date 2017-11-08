7b0267768c7d6983a5d166e403925c3fcff0831f
Ian Dickinson <i.j.dickinson@gmail.com>
2017-11-08 13:44:40 +0000

    Promote previous UkhpiIndicator struct to a full class

---
+++ app/models/ukhpi_indicator.rb
@@ -0,0 +1,45 @@
+# frozen-string-literal: true
+
+# Encapsulates an indicator for the various UKHPI statistics in the overall
+# dataset. An indicator denotes a common characteristic across the statistics,
+# such as 'average price' or 'percentage monthly change'. Indicators apply
+# to statistics to denote measures in the UKHPI data cube. Not all inidicators
+# apply to all statistics: there is no 'volume' indicator for property types
+# other than 'all property types'.
+class UkhpiIndicator
+  attr_reader :slug
+  attr_reader :root_name
+  attr_reader :label_key
+
+  def initialize(slug, root_name, label_key)
+    @slug = slug
+    @root_name = root_name
+    @label_key = label_key
+  end
+
+  # @return True if this indicator denotes sales volume
+  def volume?
+    slug == 'vol'
+  end
+
+  # @return The label for this indicator
+  def label
+    I18n.t(label_key)
+  end
+
+  # @return True if this indicator is selected in the given user selections
+  def selected?(user_selections)
+    user_selections.selected_indicators.include?(slug)
+  end
+
+  # @return The state of this indicator, serialised to a Hash
+  def to_h(user_selections)
+    {
+      slug: slug,
+      root_name: root_name,
+      label: label,
+      isVolume: volume?,
+      isSelected: selected?(user_selections)
+    }
+  end
+end
---
+++ test/models/ukhpi_indicator_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# Unit tests on the UkhpiIndicator class
+class UkhpiIndicatorTest < ActiveSupport::TestCase
+  describe 'UkhpiIndicator' do
+    describe '#initialize' do
+      it 'should provide access to the initialised values' do
+        ind = UkhpiIndicator.new('foo', 'foo_root', 'average_price')
+        ind.slug.must_equal 'foo'
+        ind.root_name.must_equal 'foo_root'
+        ind.label.must_equal 'Average price'
+      end
+    end
+
+    describe '#volume?' do
+      it 'should return correctly whether or not this is a volume indicator' do
+        refute UkhpiIndicator.new('foo', 'foo_r', 'foo_l').volume?
+        assert UkhpiIndicator.new('vol', 'foo_r', 'foo_l').volume?
+      end
+    end
+
+    describe '#to_h' do
+      it 'should correctly convert the indicator to a hash' do
+        user_selections = stub(selected_indicators: ['foo'])
+
+        hash = UkhpiIndicator.new('foo', 'r', 'average_price').to_h(user_selections)
+        hash[:slug].must_equal 'foo'
+        hash[:root_name].must_equal 'r'
+        hash[:label].must_equal 'Average price'
+        hash[:isVolume].must_equal false
+        hash[:isSelected].must_equal true
+
+        hash = UkhpiIndicator.new('bar', 'r', 'average_price').to_h(user_selections)
+        hash[:isSelected].must_equal false
+      end
+    end
+  end
+end
