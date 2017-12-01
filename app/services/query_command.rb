d59d1f465ffada0b56674cc4924be92cafbb2c1c
Ian Dickinson <i.j.dickinson@gmail.com>
2017-12-01 14:08:10 +0000

    Partial solution to #123 and #100, but can't deploy fix fully due to upstream bug on DsAPI

--- app/services/query_command.rb
+++ app/services/query_command.rb
@@ -41,6 +41,7 @@ class QueryCommand
   def build_query
     query = add_date_range_constraint(base_query)
     add_location_constraint(query)
+    # add_sort(query)
   end
 
   def api_service(service)
@@ -69,6 +70,10 @@ class QueryCommand
   def add_location_constraint(query)
     value = DataServicesApi::Value.uri(location_uri)
     query.eq('ukhpi:refRegion', value)
+  end
+
+  def add_sort(query)
+    query.sort(:up, 'ukhpi:refMonth')
   end
 
   def month_year_value(date)
