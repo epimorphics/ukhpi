dc16cb08601a650ae0a143cd863806a803c07770
Ian Dickinson <i.j.dickinson@gmail.com>
2017-12-01 17:29:23 +0000

    Fix for #132 - redirect /explore to /browse

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root 'landing#index'
   resources :landing, only: [:index]
-  get '/explore', to: 'browse#index'
+  get '/explore', to: redirect('/browse')
   resources :download, only: [:new]
   resource :browse, only: %i[show edit], controller: :browse
 
