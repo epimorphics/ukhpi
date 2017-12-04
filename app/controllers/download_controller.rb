bb7ced25f00fb75f5d289aca9c4fc8c87b250558
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-12-04 18:05:18 +0000

    WIP: progress towards issue #129 - allow download to take multiple location parameters

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -4,6 +4,17 @@
 # (csv, json, RDF etc)
 class DownloadController < ApplicationController
   def new
+    if params[:location].is_a?(Array)
+      download_multiple_locations
+    else
+      download_single_location
+    end
+  end
+
+  private
+
+  # Create a download file for just a single location
+  def download_single_location
     user_selections = UserSelections.new(params)
     query_command = QueryCommand.new(user_selections)
     query_command.perform_query
@@ -11,32 +22,44 @@ class DownloadController < ApplicationController
     format_download_results(query_command)
   end
 
-  private
+  def download_multiple_locations
+    pparams = params.permit('from', 'to', 'in', 'st', 'location' => [])
+    location_gss = pparams.delete('location')
+    locations = location_gss.map { |gss| Locations.lookup_gss(gss).uri }
 
-  def format_download_results(query_command)
+    query_commands = locations.map do |location|
+      query_params = ActionController::Parameters.new(location: location).merge(pparams)
+      QueryCommand.new(UserSelections.new(query_params))
+    end
+
+    format_download_results(query_commands)
+  end
+
+  def format_download_results(query_commands)
     respond_to do |format|
       format.json do
-        render_attachment(query_command, 'json', 'application/json')
+        render_attachment(query_commands, 'json', 'application/json')
       end
 
       format.csv do
-        render_attachment(query_command, 'csv', 'text/csv')
+        render_attachment(query_commands, 'csv', 'text/csv')
       end
     end
   end
 
-  def render_attachment(query_command, extension, mime_type)
-    filename = synthesise_filename(query_command, extension)
+  def render_attachment(query_commands, extension, mime_type)
+    filename = synthesise_filename(query_commands, extension)
     update_response_headers(filename, mime_type)
-    presenter = DownloadPresenter.new(query_command)
+    presenter = DownloadPresenter.new(query_commands)
 
     render layout: false, locals: { download_presenter: presenter }
   end
 
-  def synthesise_filename(query_command, extension)
-    summary = query_command.user_selections.summary
+  def synthesise_filename(query_commands, extension)
+    summary = query_commands.first.user_selections.summary
     file_root = summary.downcase.gsub(/\s|[[:punct:]]/, '-')
-    "ukhpi-#{file_root}.#{extension}"
+    comparison = query_commands.length > 1 ? 'comparison-' : ''
+    "ukhpi-#{comparison}#{file_root}.#{extension}"
   end
 
   def update_response_headers(filename, mime_type)
