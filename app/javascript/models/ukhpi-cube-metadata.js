6eac484a3e41fbeed3f398e168af83883e92524d
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-01-11 13:37:14 +0000

    Proposed fix for #148 - disable unavailable combinations of indicator and statistic. I added a meta-data module encapsulating the unavailable combinations of indicators and statistics. In principle, this could be auto-generated from the UPHPI model (I think), but for the time being it is hand-crafted. This model is used to disable combinations that are known in advance not to be available, e.g. sales volume for terraced houses.

---
+++ app/javascript/models/ukhpi-cube-metadata.js
@@ -0,0 +1,23 @@
+/** Meta-knowledge that we have about the UKHPI data cube. Includes:
+ * * which combinations of indicators and statistics are not available
+ */
+import _ from 'lodash';
+
+const unavailableStatInd = [
+  { statistic: 'det', indicator: 'vol' },
+  { statistic: 'sem', indicator: 'vol' },
+  { statistic: 'ter', indicator: 'vol' },
+  { statistic: 'fla', indicator: 'vol' },
+  { statistic: 'ftb', indicator: 'vol' },
+  { statistic: 'foo', indicator: 'vol' },
+];
+
+/**
+ * Return true if the given combination of statistic and indicator is not available
+ * @param  {String} stat Statistic slug
+ * @param  {String} ind  Indicator slug
+ * @return {Boolean}      True if indicator+statistic combination is not available
+ */
+export default function unavailable(stat, ind) {
+  return !!_.find(unavailableStatInd, { statistic: stat, indicator: ind });
+}
