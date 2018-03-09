3f420621fbfcb44bf67ebf226aef3fe85dcff5dc
Ian Dickinson <i.j.dickinson@gmail.com>
2018-03-09 18:35:19 +0000

    Don't log sentry errors in dev, to reduce email noise

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,4 +3,5 @@
 Raven.configure do |config|
   config.dsn = 'https://1150348b449a444bb3ac47ddd82b37c4:5fd368489fe44c0f83f1f2e5df10a7ef@sentry.io/251669'
   config.current_environment = ENV['DEPLOYMENT_ENVIRONMENT'] || Rails.env
+  config.environments = %w[production test]
 end
