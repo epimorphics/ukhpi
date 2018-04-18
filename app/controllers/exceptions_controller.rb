2f876e7ee3f2b8db4cce73b2b82e3ae28bc162b7
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-04-18 17:20:16 +0100

    Add custom error pages for reporting application errors. Fix for issue #190

---
+++ app/controllers/exceptions_controller.rb
@@ -0,0 +1,25 @@
+# frozen-string-literal: true
+
+# Controller to handle dynamically displaying error messages
+class ExceptionsController < ApplicationController
+  layout 'application'
+
+  def render_error
+    env = request.env
+    exception = env['action_dispatch.exception']
+    status_code = ActionDispatch::ExceptionWrapper.new(env, exception).status_code
+
+    sentry_code = maybe_report_to_sentry(exception, status_code)
+
+    render :error_page, locals: { status: status_code, sentry_code: sentry_code }, layout: true
+  end
+
+  private
+
+  def maybe_report_to_sentry(exception, status_code)
+    return nil unless status_code >= 500
+
+    Raven.capture_exception(exception)
+    Raven.last_event_id
+  end
+end
---
+++ config/initializers/exceptions.rb
@@ -0,0 +1,7 @@
+# frozen-string-literal: true
+
+# Custom error handling via a Rack middleware action
+Rails.application.config.exceptions_app =
+  lambda do |env|
+    ExceptionsController.action(:render_error).call(env)
+  end
