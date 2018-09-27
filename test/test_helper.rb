b4542821e0dd8a332653d639e38ac6cecc24eed7
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-09-27 11:31:49 +0100

    Fix Rubocop suggestion

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ SimpleCov.start do
 end
 
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 require 'minitest/rails/capybara'
 require 'mocha/mini_test'
