d4445d26e6be8f0145863aa007a39e2ed6e5be0b
Ian Dickinson <i.j.dickinson@gmail.com>
2019-08-16 15:44:30 +0100

    Restore module checking
    An upgrade to the overall configuration, including changes to eslint
    itself, meant that we were getting a lot of spurious "cannot resolve
    module" warnings. Solution was to remove the explicit wepback config:
    eslint seems to handle this perfectly well with default values now.

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,15 +14,7 @@ module.exports = {
   plugins: [
     'html'
   ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        // 'config': 'build/webpack.base.conf.js'
-        'config': 'config/webpack/configuration.js'
-      }
-    }
-  },
+  'settings': {},
   // add your custom rules here
   'rules': {
     // don't require .vue extension when importing
