c0cefc47fb8c5fbcb42efed42ca38ae7a39000b8
Ian Dickinson <ian.dickinson@epimorphics.com>
2019-11-18 11:35:07 +0000

    Move version from config to lib
    For consistency with the rest of the HMLR applications, we have moved
    the version ID from `config/initializers` to `app/lib`, and wrapped it
    as a Ruby module.

---
+++ app/lib/version.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Version
+  MAJOR = 1
+  MINOR = 2
+  REVISION = 4
+  VERSION = "#{MAJOR}.#{MINOR}.#{REVISION}"
+end
--- config/initializers/version.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-VERSION = '1.2.4'
-VERSION_MAJOR, VERSION_MINOR, VERSION_REV = VERSION.split('.')
