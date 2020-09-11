d733fff0426fd31a3ee346fc54e9b6d74fa0f0d7
Ian Dickinson <i.j.dickinson@gmail.com>
2020-09-11 14:23:17 +0100

    Add locale formatting for D3
    Further progress on #233 - this commit adds the locale formatting for
    D3, so that the x-axis of the indicator graphs is correctly labelled
    with Welsh month names in the Welsh locale

--- app/javascript/packs/ukhpi_vue.js
+++ app/javascript/packs/ukhpi_vue.js
@@ -1,13 +1,15 @@
 import Vue from 'vue/dist/vue.esm'
 import ElementUI from 'element-ui'
 import localeEn from 'element-ui/lib/locale/lang/en'
-import localeCy from '../lang/element-ui-cy'
+import localeElementCy from '../lang/element-ui-cy'
+import localeD3Cy from '../lang/d3-timeformat-cy.json'
 import Numeral from 'numeral'
 import * as Sentry from '@sentry/browser'
 import * as Integrations from '@sentry/integrations'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import moment from 'moment'
+import { timeFormatDefaultLocale } from 'd3-time-format'
 
 import router from '../router/index.js.erb'
 import store from '../store/index'
@@ -23,7 +25,7 @@ import i18n from 'lang'
 Vue.use(VueI18n)
 
 // Use Element.IO
-Vue.use(ElementUI, { locale: i18n.locale === 'en' ? localeEn : localeCy })
+Vue.use(ElementUI, { locale: i18n.locale === 'en' ? localeEn : localeElementCy })
 
 // locale settings
 Numeral.register('locale', 'gb', {
@@ -38,7 +40,13 @@ Numeral.register('locale', 'gb', {
 })
 Numeral.locale('gb')
 
+// other i18n settings: moment.js
 moment.locale(i18n.locale)
+
+// other i18n settings: D3.js
+if (i18n.locale === 'cy') {
+  timeFormatDefaultLocale(localeD3Cy)
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   if (process.env.NODE_ENV === 'production') {
