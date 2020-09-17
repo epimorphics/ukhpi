6bcd679ebb15a132057697282abfdcd5a836bec3
Ian Dickinson <i.j.dickinson@gmail.com>
2020-09-17 17:32:46 +0100

    Trap focus in modal dialogues
    As contribution to #254, this commit adds a wrapper element that will
    trap the focus within a displayed modal dialogue. Prior to this change,
    a user of keyboard navigation would tab from the last active element in
    the dialog to the next focusable element in the background application.
    With the focus trap enabled, tabbing from the last active element will
    return focus to the first active element of the dialog, and conversely
    shift-tab from the first active element will cycle back to the last
    active element in the form
    
    Also logged as ticket #250

--- app/javascript/components/data-view-dates.vue
+++ app/javascript/components/data-view-dates.vue
@@ -1,67 +1,75 @@
 <template lang='html'>
-  <div class='c-options-selection__dates'>
-    <span class='' v-if='fromDate'>
-      <el-button
-        @click='onChangeDates'
-        class='c-options-selection__button'
-        :title='$t("js.dates_picker.select_dates")'
-      >
-        {{ fromDateFormatted }}
-        {{ $t('preposition.to') }}
-        {{ toDateFormatted }}
-        <i class='fa fa-edit'></i>
-      </el-button>
-    </span>
-
-    <el-dialog
-      :title='$t("js.dates_picker.date_range_prompt")'
-      :visible.sync='dialogVisible'
-      :show-close='true'
-    >
-      <el-row>
-        <el-col :span='12'>
-          <label>
-            {{ $t("js.dates_picker.start") }}<span class='u-sr-only'>{{ $t('browse.edit.form.dates_format_sr') }}</span>:
-            <el-date-picker
-              v-model='newFromDate'
-              type='month'
-              :placeholder='$t("js.compare.dates_from")'>
-            </el-date-picker>
-          </label>
-        </el-col>
-        <el-col :span='12'>
-          <label>
-            {{ $t("js.dates_picker.end") }}<span class='u-sr-only'>{{ $t('browse.edit.form.dates_format_sr') }}</span>:
-            <el-date-picker
-              v-model='newToDate'
-              type='month'
-              :placeholder='$t("js.compare.dates_to")'>
-            </el-date-picker>
-          </label>
-        </el-col>
-      </el-row>
-      <el-row>
-        <p v-if='validationMessage'>
-          <el-alert
-            :title='validationMessage'
-            type='warning'>
-          </el-alert>
-        </p>
-      </el-row>
-      <span slot='footer' class='dialog-footer'>
-        <el-button @click='dialogVisible = false'>{{ $t("js.action.cancel") }}</el-button>
-        <el-button type='primary' @click='onSaveChanges'>{{ $t("js.action.confirm") }}</el-button>
+  <focus-trap v-model='dialogVisible' :initial-focus='initialFocusElement'>
+    <div class='c-options-selection__dates'>
+      <span class='' v-if='fromDate'>
+        <el-button
+          @click='onChangeDates'
+          class='c-options-selection__button'
+          :title='$t("js.dates_picker.select_dates")'
+        >
+          {{ fromDateFormatted }}
+          {{ $t('preposition.to') }}
+          {{ toDateFormatted }}
+          <i class='fa fa-edit'></i>
+        </el-button>
       </span>
-    </el-dialog>
-  </div>
+
+      <el-dialog
+        :title='$t("js.dates_picker.date_range_prompt")'
+        :visible.sync='dialogVisible'
+        :show-close='true'
+      >
+        <el-row>
+          <el-col :span='12'>
+            <label>
+              {{ $t("js.dates_picker.start") }}<span class='u-sr-only'>{{ $t('browse.edit.form.dates_format_sr') }}</span>:
+              <el-date-picker
+                v-model='newFromDate'
+                type='month'
+                :placeholder='$t("js.compare.dates_from")'
+                ref='fromDatePicker'
+              ></el-date-picker>
+            </label>
+          </el-col>
+          <el-col :span='12'>
+            <label>
+              {{ $t("js.dates_picker.end") }}<span class='u-sr-only'>{{ $t('browse.edit.form.dates_format_sr') }}</span>:
+              <el-date-picker
+                v-model='newToDate'
+                type='month'
+                :placeholder='$t("js.compare.dates_to")'>
+              </el-date-picker>
+            </label>
+          </el-col>
+        </el-row>
+        <el-row>
+          <p v-if='validationMessage'>
+            <el-alert
+              :title='validationMessage'
+              type='warning'>
+            </el-alert>
+          </p>
+        </el-row>
+        <span slot='footer' class='dialog-footer'>
+          <el-button @click='dialogVisible = false'>{{ $t("js.action.cancel") }}</el-button>
+          <el-button type='primary' @click='onSaveChanges'>{{ $t("js.action.confirm") }}</el-button>
+        </span>
+      </el-dialog>
+    </div>
+  </focus-trap>
 </template>
 
 <script>
 import Moment from 'moment';
+import { FocusTrap } from 'focus-trap-vue'
 import { SET_DATES } from '../store/mutation-types';
 import bus from '../lib/event-bus';
 
 export default {
+  components: {
+    FocusTrap
+  },
+
   data: () => ({
     newFromDate: null,
     newToDate: null,
@@ -88,6 +96,10 @@ export default {
   },
 
   methods: {
+    initialFocusElement () {
+      return this.$refs.fromDatePicker
+    },
+
     onChangeDates() {
       this.newFromDate = this.fromDate;
       this.newToDate = this.toDate;
--- app/javascript/components/select-location.vue
+++ app/javascript/components/select-location.vue
@@ -1,94 +1,98 @@
 <template lang='html'>
-  <div class='o-select-location'>
-    <el-dialog
-      :title='title'
-      :visible.sync='showDialog'
-      :show-close='true'
-      @close='notifyDialogClosed'
-    >
-      <el-row>
-        <el-col :span='24'>
-          <p>
-            {{ prompt }}
-          </p>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span='24'>
-          <label>
-            {{ $t('js.location.search_locations_label') }}
-            <el-input
-              v-model='searchInput'
-              @change='onSearchInput'
-              @keyup.native='onSearchKeyUp'
-              :autofocus='true'
-            >
-            </el-input>
-          </label>
-        </el-col>
-        <el-col :span='24'>
-          <el-popover
-            placement='bottom'
-            :title='$t("js.location.results")'
-            width='400'
-            trigger='manual'
-            v-model='searchResultsVisible'
-          >
-            <ul v-for='result in searchResults' class='o-search-location__results'>
-              <li class='o-search-location__result'>
-                <el-button type='text' @click='onSelectResult'>
-                  {{ result.labels[$locale] }}
-                </el-button>
-                ({{ result | locationTypeLabel }})
-              </li>
-            </ul>
-            <p v-if='manyResults > 0'>
-              {{ $t("js.location.many_results", { manyResults }) }}
+  <focus-trap v-model='showDialog' :initial-focus='initialFocusElement'>
+    <div class='o-select-location'>
+      <el-dialog
+        :title='title'
+        :visible.sync='showDialog'
+        :show-close='true'
+        @close='notifyDialogClosed'
+      >
+        <el-row>
+          <el-col :span='24'>
+            <p>
+              {{ prompt }}
             </p>
-          </el-popover>
-        </el-col>
-        <el-alert type='warning' :title='noMatch' v-if='noMatch'></el-alert>
-      </el-row>
+          </el-col>
+        </el-row>
 
-      <el-row>
-        <p v-if='validationMessage'>
-          <el-alert
-            :title='validationMessage'
-            type='warning'>
-          </el-alert>
-        </p>
-      </el-row>
+        <el-row>
+          <el-col :span='24'>
+            <label>
+              {{ $t('js.location.search_locations_label') }}
+              <el-input
+                v-model='searchInput'
+                @change='onSearchInput'
+                @keyup.native='onSearchKeyUp'
+                :autofocus='true'
+                ref='searchInputField'
+              >
+              </el-input>
+            </label>
+          </el-col>
+          <el-col :span='24'>
+            <el-popover
+              placement='bottom'
+              :title='$t("js.location.results")'
+              width='400'
+              trigger='manual'
+              v-model='searchResultsVisible'
+            >
+              <ul v-for='result in searchResults' class='o-search-location__results'>
+                <li class='o-search-location__result'>
+                  <el-button type='text' @click='onSelectResult'>
+                    {{ result.labels[$locale] }}
+                  </el-button>
+                  ({{ result | locationTypeLabel }})
+                </li>
+              </ul>
+              <p v-if='manyResults > 0'>
+                {{ $t("js.location.many_results", { manyResults }) }}
+              </p>
+            </el-popover>
+          </el-col>
+          <el-alert type='warning' :title='noMatch' v-if='noMatch'></el-alert>
+        </el-row>
 
-      <el-row>
-        <div class='c-map'>
-          <div>
-            {{ $t("js.location.show_on_map") }}
-            <el-radio-group v-model='locationType'>
-              <el-radio-button label='country'>{{ $t("js.location.type_countries") }}</el-radio-button>
-              <el-radio-button label='la'>{{ $t("js.location.type_las") }}</el-radio-button>
-              <el-radio-button label='region'>{{ $t("js.location.type_regions_england") }}</el-radio-button>
-              <el-radio-button label='county'>{{ $t("js.location.type_counties_england") }}</el-radio-button>
-            </el-radio-group>
+        <el-row>
+          <p v-if='validationMessage'>
+            <el-alert
+              :title='validationMessage'
+              type='warning'>
+            </el-alert>
+          </p>
+        </el-row>
+
+        <el-row>
+          <div class='c-map'>
+            <div>
+              {{ $t("js.location.show_on_map") }}
+              <el-radio-group v-model='locationType'>
+                <el-radio-button label='country'>{{ $t("js.location.type_countries") }}</el-radio-button>
+                <el-radio-button label='la'>{{ $t("js.location.type_las") }}</el-radio-button>
+                <el-radio-button label='region'>{{ $t("js.location.type_regions_england") }}</el-radio-button>
+                <el-radio-button label='county'>{{ $t("js.location.type_counties_england") }}</el-radio-button>
+              </el-radio-group>
+            </div>
+            <div :id='mapElementId'></div>
           </div>
-          <div :id='mapElementId'></div>
-        </div>
-      </el-row>
+        </el-row>
 
-      <span slot='footer' class='dialog-footer'>
-        <el-button @click='onHideDialog'>{{ $t('js.action.cancel') }}</el-button>
-        <el-button
-          type='primary'
-          @click='onSaveChanges'
-          :disabled='!allowConfirm'
-        >{{ $t('js.action.confirm') }}</el-button>
-      </span>
-    </el-dialog>
-  </div>
+        <span slot='footer' class='dialog-footer'>
+          <el-button @click='onHideDialog'>{{ $t('js.action.cancel') }}</el-button>
+          <el-button
+            type='primary'
+            @click='onSaveChanges'
+            :disabled='!allowConfirm'
+          >{{ $t('js.action.confirm') }}</el-button>
+        </span>
+      </el-dialog>
+    </div>
+  </focus-trap>
 </template>
 
 <script>
 import _ from 'lodash';
+import { FocusTrap } from 'focus-trap-vue'
 import { locationIndexType, locationsNamed, findLocationById } from '../lib/locations';
 import LocationsMap from '../presenters/locations-map';
 import { SET_LOCATION } from '../store/mutation-types';
@@ -97,6 +101,10 @@ import bus from '../lib/event-bus';
 const MAX_RESULTS = 10;
 
 export default {
+  components: {
+    FocusTrap
+  },
+
   data: () => ({
     showDialog: false,
     locationType: 'country',
@@ -186,6 +194,10 @@ export default {
       this.manyResults = 0;
       this.searchResults = [];
       this.noMatch = null
+    },
+
+    initialFocusElement () {
+      return this.$refs.searchInputField
     },
 
     onSaveChanges() {
