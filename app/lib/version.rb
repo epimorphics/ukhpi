59e864a59f6f6f3ba0737aad6a36104a261ad741
Ian Dickinson <i.j.dickinson@gmail.com>
2020-10-05 10:26:39 +0100

    Update version and changelog

--- app/lib/version.rb
+++ app/lib/version.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Version
-  MAJOR = '1'
-  MINOR = '5'
-  REVISION = '9'
+  MAJOR = 1
+  MINOR = 5
+  REVISION = 10
   VERSION = "#{MAJOR}.#{MINOR}.#{REVISION}"
 end
