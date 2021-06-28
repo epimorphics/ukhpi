2cacae71a03ff842b8664a490bbed5163b363cec
bogdan <marc.bogdan96@yahoo.com>
2021-06-28 11:54:44 +0100

    Old logger formatter removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  # config.log_formatter = ::Logger::Formatter.new
 
   # Disable logging of the rendering of partials
   config.action_view.logger = nil
