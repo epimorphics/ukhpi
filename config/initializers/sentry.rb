14376d60ae6f54b3b3bf722aaf41a898a10c9875
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-01-17 13:50:57 +0000

    ci: Removal of test environment to reduce sentry monitoring charges

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,7 +5,7 @@ Rails.application.reloader.to_prepare do
     Sentry.init do |config|
       config.dsn = ENV['SENTRY_API_KEY']
       config.environment = ENV.fetch('DEPLOYMENT_ENVIRONMENT') { Rails.env }
-      config.enabled_environments = %w[production test]
+      config.enabled_environments = %w[production]
       config.release = Version::VERSION
       config.breadcrumbs_logger = %i[active_support_logger http_logger]
       config.excluded_exceptions += ['ActionController::BadRequest']
