ae07ec6b444dccabfcf3d7acb8dd710e181b1a3a
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-01-27 09:28:14 +0000

    build: Updated version props
    Refactored the version cadence creation to include the `PATCH` property as per other Epimorphics apps.

--- app/lib/version.rb
+++ app/lib/version.rb
@@ -3,7 +3,7 @@
 module Version
   MAJOR = 1
   MINOR = 5
-  REVISION = 20
+  PATCH = 20
   SUFFIX = nil
-  VERSION = "#{MAJOR}.#{MINOR}.#{REVISION}#{SUFFIX && ".#{SUFFIX}"}"
+  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && ".#{SUFFIX}"}"
 end
