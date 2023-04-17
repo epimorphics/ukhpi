59254f82b973834069c515743a46a6cb7e35ff69
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-04-17 11:48:43 +0100

    Updated `version.rb` to include `SUFFIX` parameter for improved release cadence

--- app/lib/version.rb
+++ app/lib/version.rb
@@ -4,5 +4,6 @@ module Version
   MAJOR = 1
   MINOR = 5
   REVISION = 20
-  VERSION = "#{MAJOR}.#{MINOR}.#{REVISION}"
+  SUFFIX = nil
+  VERSION = "#{MAJOR}.#{MINOR}.#{REVISION}#{SUFFIX && ".#{SUFFIX}"}"
 end
