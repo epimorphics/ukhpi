cb186f1b64142b62fa7c3a3e8b9cd16d2f5fa82f
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-06-29 15:00:21 +0100

    Feat: Continued logging improvements
    Initial steps to include more details in the logging messages as well as handles the specific status levels with the appropriate logging type.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,37 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def create_detailed_request_log(duration)
+    env = request.env
+
+    log_fields = {
+      duration: duration,
+      request_id: env['X_REQUEST_ID'],
+      forwarded_for: env['X_FORWARDED_FOR'],
+      path: env['REQUEST_PATH'],
+      query_string: env['QUERY_STRING'],
+      user_agent: env['HTTP_USER_AGENT'],
+      accept: env['HTTP_ACCEPT'],
+      body: request.body.gets&.gsub("\n", '\n'),
+      method: request.method,
+      status: response.status,
+      message: Rack::Utils::HTTP_STATUS_CODES[response.status]
+    }
+
+    case response.status
+    when 500..599
+      log_fields[:message] = env['action_dispatch.exception']
+      Rails.logger.error(JSON.generate(log_fields))
+    when 400..499
+      Rails.logger.warn(JSON.generate(log_fields))
+    else
+      Rails.logger.info(JSON.generate(log_fields))
+    end
+  end
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+
   # Set the user's preferred locale. An explicit locale set via
   # the URL param `lang` is preeminent, otherwise we look to the
   # user's preferred language specified via browser headers
