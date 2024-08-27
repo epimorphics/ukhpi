cc0322b0ffc73c0cfd1adcee9035bfea951daf75
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-08-27 13:53:02 +0100

    refactor: adjusted code layout for readability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_locale, :change_default_caching_policy
+  before_action :set_locale
+  before_action :change_default_caching_policy
 
   around_action :log_request_result
+
+  # Set the user's preferred locale. An explicit locale set via
+  # the URL param `lang` is preeminent, otherwise we look to the
+  # user's preferred language specified via browser headers
+  def set_locale
+    user_locale = params['lang']
+    user_locale ||= http_accept_language.compatible_language_from(I18n.available_locales)
+
+    I18n.locale = user_locale if Rails.application.config.welsh_language_enabled
+  end
+
+  # * Set cache control headers for HMLR apps to be public and cacheable
+  # * UHPI needs to be shorter to avoid delay (in users cache) on the
+  # * publication deadline so it is set for 2 minutes (120 seconds)
+  # Sets the default `Cache-Control` header for all requests,
+  # unless overridden in the action
+  def change_default_caching_policy
+    expires_in 2.minutes, public: true, must_revalidate: true if Rails.env.production?
+  end
+
   def log_request_result
     start = Process.clock_gettime(Process::CLOCK_MONOTONIC, :microsecond)
     yield
@@ -38,7 +59,7 @@ class ApplicationController < ActionController::Base
 
     case response.status
     when 500..599
-      log_fields[:message] = env['action_dispatch.exception']
+      log_fields[:message] = env['action_dispatch.exception'].to_s
       Rails.logger.error(JSON.generate(log_fields))
     when 400..499
       Rails.logger.warn(JSON.generate(log_fields))
@@ -48,22 +69,12 @@ class ApplicationController < ActionController::Base
   end
   # rubocop:enable Metrics/AbcSize
 
-  # Set the user's preferred locale. An explicit locale set via
-  # the URL param `lang` is preeminent, otherwise we look to the
-  # user's preferred language specified via browser headers
-  def set_locale
-    user_locale = params['lang']
-    user_locale ||= http_accept_language.compatible_language_from(I18n.available_locales)
-
-    I18n.locale = user_locale if Rails.application.config.welsh_language_enabled
-  end
-
-  # * Set cache control headers for HMLR apps to be public and cacheable
-  # * UHPI needs to be shorter to avoid delay (in users cache) on the
-  # * publication deadline so it is set for 2 minutes (120 seconds)
-  # Set the default `Cache-Control` header for all requests,
-  # unless overridden in the action
-  def change_default_caching_policy
-    expires_in 2.minutes, public: true, must_revalidate: true if Rails.env.production?
+  # Notify subscriber(s) of an internal error event with the payload of the
+  # exception once done
+  # @param [Exception] exp the exception that caused the error
+  # @return [ActiveSupport::Notifications::Event] provides an object-oriented
+  # interface to the event
+  def instrument_internal_error(exception)
+    ActiveSupport::Notifications.instrument('internal_error.application', exception: exception)
   end
 end
