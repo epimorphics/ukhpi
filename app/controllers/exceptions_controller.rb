5322aa4e9681c706115bf801fbdb0b4eac5af3a8
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-10-09 11:02:07 +0100

    refactor: adjusted internal error instrumentation
    Set the Internal Error Instrumentation to an `unless` statement to ensure the application does not report internal errors to the Prometheus metrics when the error is a 404 thereby reducing the noise in the Slack alerts channel

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -10,8 +10,9 @@ class ExceptionsController < ApplicationController
     status_code = ActionDispatch::ExceptionWrapper.new(env, exception).status_code
 
     sentry_code = maybe_report_to_sentry(exception, status_code)
-    # add the exception to the prometheus metrics
-    instrument_internal_error(exception)
+
+    # add the exception to the prometheus metrics but only on errors that are 404
+    instrument_internal_error(exception) unless status_code == 404
 
     render :error_page,
            locals: { status: status_code, sentry_code: sentry_code },
@@ -22,7 +23,7 @@ class ExceptionsController < ApplicationController
   private
 
   def maybe_report_to_sentry(exception, status_code)
-    return nil if Rails.env.development? # Why are we reporting to Senty in dev?
+    return nil if Rails.env.development? # Why are we reporting to Sentry in dev?
     return nil unless status_code >= 500
 
     sevent = Sentry.capture_exception(exception)
