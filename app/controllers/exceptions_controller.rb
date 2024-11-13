1e30fff080a0e843a76eeaffcac7bf135499dc50
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-11-13 10:13:14 +0000

    fix: moved `instrument_internal_error` method
    to ensure the metric is only triggered on errors with a 500 status or greater

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -11,9 +11,6 @@ class ExceptionsController < ApplicationController
 
     sentry_code = maybe_report_to_sentry(exception, status_code)
 
-    # add the exception to the prometheus metrics but only on errors that are 404
-    instrument_internal_error(exception) unless status_code == 404
-
     render :error_page,
            locals: { status: status_code, sentry_code: sentry_code },
            layout: true,
@@ -25,6 +22,9 @@ class ExceptionsController < ApplicationController
   def maybe_report_to_sentry(exception, status_code)
     return nil if Rails.env.development? # Why are we reporting to Sentry in dev?
     return nil unless status_code >= 500
+
+    # Trigger error metrics on 5xx errors only to reduce slack noise
+    instrument_internal_error(exception)
 
     sevent = Sentry.capture_exception(exception)
     sevent&.event_id
