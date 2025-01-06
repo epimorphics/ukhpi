97451b9135c023eea673f029725e3f190fa97adc
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-01-06 12:23:42 +0000

    fix: better error control
    Handle specific types of exceptions and render the appropriate error page or attempt to render a generic error page if no specific error page exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 
 # :nodoc:
 class ApplicationController < ActionController::Base
+  include Rails.application.routes.url_helpers
+  include ActionView::Helpers::TranslationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -35,6 +37,76 @@ class ApplicationController < ActionController::Base
     yield
     duration = Process.clock_gettime(Process::CLOCK_MONOTONIC, :microsecond) - start
     detailed_request_log(duration)
+  end
+
+  # Handle specific types of exceptions and render the appropriate error page
+  # or attempt to render a generic error page if no specific error page exists
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from StandardError do |e|
+      # Trigger the appropriate error handling method based on the exception
+      case e.class
+      when ActionController::RoutingError, ActionView::MissingTemplate
+        :render404
+      when ActionController::InvalidCrossOriginRequest
+        :render403
+      when ActionController::BadRequest, ActionController::ParameterMissing
+        :render400
+      else
+        :handle_internal_error
+      end
+    end
+  end
+
+  # Render the appropriate error page based on the exception
+  def handle_internal_error(exception)
+    if exception.instance_of? ArgumentError
+      render_error(400)
+    else
+      Rails.logger.warn "No explicit error page for exception #{exception} - #{exception.class}"
+      # Instrument ActiveSupport::Notifications for internal server errors only:
+      sentry_code = instrument_internal_error(exception)
+      render_error(500, sentry_code)
+    end
+  end
+
+  def render_400(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(400)
+  end
+
+  def render_403(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(403)
+  end
+
+  def render_404(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(404)
+  end
+
+  def render_500(_exception = nil) # rubocop:disable Naming/VariableNumber
+    render_error(500)
+  end
+
+  def render_error(status, sentry_code = nil) # rubocop:disable Metrics/AbcSize
+    reset_response
+
+    status = Rack::Utils::SYMBOL_TO_STATUS_CODE[status] if status.is_a?(Symbol)
+    @view_state ||= LandingState.new(UserLanguageSelection.new(params))
+
+    respond_to do |format|
+      format.html { render_html_error_page(status, sentry_code) }
+      # Anything else returns the status as human readable plain string
+      format.all { render plain: Rack::Utils::HTTP_STATUS_CODES[status].to_s, status: status }
+    end
+  end
+
+  def render_html_error_page(status, sentry_code) # rubocop:disable Metrics/MethodLength
+    render 'exceptions/error_page',
+           locals: { status: status, sentry_code: sentry_code },
+           layout: true,
+           status: status
+  end
+
+  def reset_response
+    self.response_body = nil
   end
 
   private
