87e162e3a959d04453a450fa10d1ade6884c028d
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-02-21 14:19:25 +0000

    refactor: Improve logging functionality
    - Enhanced request logging with clearer defaults.
    - Simplified extraction of service and params.
    - Added handling for request time in log messages.
    - Cleaned up nil values before logging.

--- app/lib/logging_helper.rb
+++ app/lib/logging_helper.rb
@@ -4,18 +4,35 @@
 class LoggingHelper
   def self.log_request(fields, type = 'info') # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     puts "\n" if Rails.env.development? && Rails.logger.debug? # rubocop:disable Rails/Output
-
     # Extract service and params from fields hash
-    service = fields.delete(:service) if fields.key(:service)
-    params = fields.delete(:params) if fields.key(:params)
-
-    fields[:message] ||= 'Received Data Services API request from the UKHPI service'
+    service = fields[:service] if fields[:service].respond_to?(:data_api)
+    fields[:params].presence
+    # Set the service and params keys to nil for later removal
+    fields[:service] = nil
+    fields[:params] = nil
+    # Set default values for log fields
+    fields[:message] ||= 'Received request'
     fields[:method] ||= 'GET'
-    fields[:path] ||= URI.parse(service.data_api).path if service
-    # fields[:request_path] ||= URI.parse(service.data_api).path if service
-    fields[:query_string] ||= JSON.generate(params) if params
+
+    fields[:path] ||= nil
+    if service.respond_to?(:data_api) && fields[:path].blank?
+      fields[:path] = URI.parse(service.data_api).path
+      # elsif params
+      #   fields[:path] += "?#{JSON.generate(params)}"
+    end
+
+    fields[:message] += ": #{fields[:path]}" if fields[:path]
+
     fields[:request_status] ||= 'received'
+    fields[:request_time] ||= fields[:duration]
+
+    if fields[:request_time]
+      fields[:message] += format(', time taken: %.0f ms', fields[:request_time])
+    end
+
     fields[:status] ||= Rack::Utils::SYMBOL_TO_STATUS_CODE[:ok]
+
+    fields.compact! # Finally, remove any nil values before sending to the logger
 
     Rails.logger.send(type) { JSON.generate(fields.sort.to_h) } unless fields.empty?
     Rails.logger.flush if Rails.logger.respond_to?(:flush)
