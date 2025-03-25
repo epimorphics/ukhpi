9237000c7cb46d67b9f6d852de1848840149d5ad
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-03-25 17:54:42 +0000

    refactor: update import paths for consistency
    - Changed relative imports to absolute imports for clarity
    - Updated multiple components to use the new import style
    - Maintained functionality while improving code readability

--- app/javascript/components/data-view.vue
+++ app/javascript/components/data-view.vue
@@ -59,13 +59,13 @@ import DataViewStatistics from './data-view-statistics.vue';
 import DataViewTable from './data-view-table.vue';
 import DataViewGraph from './data-view-graph.vue';
 import DataViewDownload from './data-view-download.vue';
-import store from '../store/index';
-import { INITIALISE, SELECT_STATISTIC } from '../store/mutation-types';
-import bus from '../lib/event-bus';
-import safeForEach from '../lib/safe-foreach';
-import AvailableStatistics from '../mixins/available-statistics';
+import store from '@/store/index';
+import { INITIALISE, SELECT_STATISTIC } from '@/store/mutation-types';
+import bus from '@/lib/event-bus';
+import safeForEach from '@/lib/safe-foreach';
+import AvailableStatistics from '@/mixins/available-statistics';
 import VueI18n from '../lang'
-import { mutateName } from 'lang/welsh-name-mutations'
+import { mutateName } from '@/lang/welsh-name-mutations'
 
 export default {
   mixins: [AvailableStatistics],
