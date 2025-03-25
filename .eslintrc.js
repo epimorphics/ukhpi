c6f8d374b406c69c1730f8566888243fd5d8b6f5
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-03-25 17:56:50 +0000

    style: update ESLint config for dependencies
    - Commented out the rule for extraneous dependencies
    - Kept the debugger rule based on environment settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,9 +23,9 @@ module.exports = {
       'vue': 'never'
     }],
     // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
+    // 'import/no-extraneous-dependencies': ['error', {
+    //   'optionalDependencies': ['test/unit/index.js']
+    // }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
