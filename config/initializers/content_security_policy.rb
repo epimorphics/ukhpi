06d015ded30170043100fa15d77df82add562f9a
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-03-28 11:06:51 +0000

    feat: add worker source to csp for Sentry integration
    - Added `worker_src` directive for blob support to content security policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -25,6 +25,7 @@ Rails.application.config.content_security_policy do |policy|
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
   policy.script_src :self, :https, :unsafe_inline, :unsafe_eval
+  policy.worker_src :self, :blob
 end
 
 # If you are using UJS then enable automatic nonce generation
