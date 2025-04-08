6addc9b13c68b3a06b19b158c65d634b91b89bb4
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-04-08 13:01:16 +0100

    Style: Enforces line length with rubocop
    Enforces line length consistency in the content security policy configuration using rubocop.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,14 +6,16 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
+# rubocop:disable Layout/LineLength
 Rails.application.config.content_security_policy do |policy|
   # policy.default_src :self, :https
   # policy.font_src    :self, :https, :data
   # policy.img_src     :self, :https, :data
   # policy.object_src  :none
   # policy.script_src  :self, :https
+
   # Allow @vite/client to hot reload javascript changes in development
-  # policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development? # rubocop:disable Layout/LineLength
+  # policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
 
   # You may need to enable this in production as well depending on your setup.
   # policy.script_src *policy.script_src, :blob if Rails.env.test?
@@ -35,3 +37,5 @@ end
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+
+# rubocop:enable Layout/LineLength
