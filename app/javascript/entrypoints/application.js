a7b6ac715d8d9c5d964fcd6b21d30264f4283f61
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-06-16 07:33:38 -0400

    chore: update basepath for environments
    - Use a dynamic base URL for static asset paths
    - Improve configuration flexibility by using environmental settings

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -32,6 +32,7 @@ console.debug(`Project: ${import.meta.env.SENTRY_PROJECT}`)
 console.debug(`Rails environment: ${import.meta.env.RAILS_ENV}`)
 console.debug(`Node environment: ${import.meta.env.MODE}`)
 console.debug(`HMLR UKHPI Environment: ${import.meta.env.SENTRY_ENVIRONMENT}`)
+console.debug('HMLR UKHPI Basepath:', import.meta.env.RAILS_RELATIVE_URL_ROOT)
 console.debug(`HMLR UKHPI Version: ${currentAppRelease}`)
 console.debug(`Log Level: ${import.meta.env.LOG_LEVEL}`)
 
--- vite.config.mts
+++ vite.config.mts
@@ -37,7 +37,7 @@ export default defineConfig(({ command, mode }) => {
     ],
     build: {
       assetsInlineLimit: 0, // Prevents inlining of all assets
-      base: env.RAILS_ENV === 'production' ? '/app/ukhpi/' : '/',
+      base: env.RAILS_RELATIVE_URL_ROOT,
       sourcemap: true, // Source map generation must be turned on for Sentry to work
       target: 'esnext'
     },
