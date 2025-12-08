0667b612b584613cd632f9b732178fbc27c64931
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-12-08 18:05:03 +0000

    style: reformat code for consistency

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -1,26 +1,26 @@
-import Vue from 'vue/dist/vue.esm';
-import Router from 'vue-router';
-import SingleLocation from '@/components/single-location.vue';
-import CompareLocations from '@/components/compare-locations.vue';
+import Vue from 'vue/dist/vue.esm'
+import Router from 'vue-router'
+import SingleLocation from '@/components/single-location.vue'
+import CompareLocations from '@/components/compare-locations.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 // # This is a workaround to avoid the issue of Vue Router not being able to handle
 // # relative URLs correctly when using Rails' asset pipeline.
-const relativeUrlRoot = window.ukhpi.root_path || '';
+const relativeUrlRoot = window.ukhpi.root_path || ''
 
 export default new Router({
   routes: [
     {
       path: `${relativeUrlRoot}/browse`,
       name: 'single-location',
-      component: SingleLocation
+      component: SingleLocation,
     },
     {
       path: `${relativeUrlRoot}/compare`,
       name: 'compare-locations',
-      component: CompareLocations
-    }
+      component: CompareLocations,
+    },
   ],
-  mode: 'history'
-});
+  mode: 'history',
+})
