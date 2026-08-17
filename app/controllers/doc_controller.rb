94d7540eff1021a7e5291d680617bf3320d840ff
joescottdave <joseph.davies@outlook.com>
2026-08-17 18:36:30 +0100

    refactor(logging): remove redundant log lines

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -1,22 +1,18 @@
 # Simple controller for serving documentation
 class DocController < ApplicationController
   def index
-    Log.info('Requesting Doc Controller', { params: params, path: request.path })
     @view_state = LanguageState.new(UserLanguageSelection.new(params))
   end
 
   def ukhpi
-    Log.info('Requesting UKHPI Documentation', { params: params, path: request.path })
     @view_state = LanguageState.new(UserLanguageSelection.new(params))
   end
 
   def ukhpi_dsd
-    Log.info('Requesting UKHPI DSD Documentation', { params: params, path: request.path })
     send_file 'app/views/doc/ukhpi-dsd.ttl', type: 'text/turtle'
   end
 
   def ukhpi_user_guide
-    Log.info('Requesting UKHPI User Guide', { params: params, path: request.path })
     send_file(
       'app/views/doc/ukhpi-user-guide.pdf',
       type: 'application/pdf',
