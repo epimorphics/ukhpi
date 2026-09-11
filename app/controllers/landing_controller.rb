5b5adff518ee70e53d61537c146e3fd2544b047d
joescottdave <joseph.davies@outlook.com>
2026-09-11 18:28:10 +0100

    refactor: remove per action request log lines

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,6 @@
 # :nodoc:
 class LandingController < ApplicationController
   def index
-    Log.info('Requesting Landing Controller', { params: params, path: request.path })
     @view_state = LandingState.new(UserLanguageSelection.new(params))
   end
 end
